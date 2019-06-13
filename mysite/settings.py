ec07e2f0dd639ff82c249fe759e6307904cfe48b
John Blischak <jdblischak@gmail.com>
2019-06-13 11:15:42 -0400

    Chapter 02: Share blog post by email

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Write emails to the console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Send emails using the Gmail SMTP Server
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'your_account@gmail.com'
+# EMAIL_HOST_PASSWORD = 'your_password'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
