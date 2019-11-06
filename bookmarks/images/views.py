6da1f8e8654b7d2cef386abcc8f7767e97f97aaa
John Blischak <jdblischak@gmail.com>
2019-11-06 09:48:34 -0500

    Chapter 06: Storing a ranking in Redis
    Had to fix typo in redis.Redis.zincrby() for the ranking page to work.
    
    https://redis-py.readthedocs.io/en/latest/index.html#redis.Redis.zincrby
    https://github.com/PacktPublishing/Django-2-by-Example/pull/33

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -50,6 +50,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
     total_views = r.incr('image:{}:views'.format(image.id))
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
@@ -101,3 +103,17 @@ def image_list(request):
     return render(request,
                   'images/image/list.html',
                   {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
