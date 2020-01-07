7cdddef5b5db6985e69c7e19dec5e08d6985fa34
John Blischak <jdblischak@gmail.com>
2020-01-07 11:39:34 -0500

    Django Tutorial: Part 7, customizing the admin form

--- django-tutorial-polls/mysite/polls/admin.py
+++ django-tutorial-polls/mysite/polls/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(models.Question, QuestionAdmin)
 admin.site.register(models.Choice)
